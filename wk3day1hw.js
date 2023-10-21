c6dae1f3975bc02cabe71c93569765d3924dd655
sahomelabs <sahomelabs@gmail.com>
2023-10-21 11:14:22 -0400

    complete lab1

---
+++ wk3day1hw.js
@@ -0,0 +1,15 @@
+// 1
+function maxOfTwoNumbers(number1, number2) {
+  if (number1 >= number2) {
+    return number1;
+  } else  {
+    return number2;
+    }
+}
+console.log(maxOfTwoNumbers(10, 5));
+
+
+
+
+
+  
