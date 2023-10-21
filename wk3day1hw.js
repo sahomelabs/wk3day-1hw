ce7cd82b3fd42cd4da3ef3f1325bcbd1888e2c29
sahomelabs <sahomelabs@gmail.com>
2023-10-21 11:45:56 -0400

    Complete Lab2

--- wk3day1hw.js
+++ wk3day1hw.js
@@ -1,15 +1,27 @@
 // 1
-function maxOfTwoNumbers(number1, number2) {
-  if (number1 >= number2) {
-    return number1;
-  } else  {
-    return number2;
-    }
+// function maxOfTwoNumbers(number1, number2) {
+//   if (number1 >= number2) {
+//     return number1;
+//   } else  {
+//     return number2;
+//     }
+// }
+// console.log(maxOfTwoNumbers(10, 5));
+
+
+// 2. 
+function maxOfThree(number1, number2, number3) {
+  let max = number1;
+
+  if (number2 > max) {
+    max = number2;
+  }
+
+  if (number3 > max) {
+    max = number3;
+  }
+
+  return max;
 }
-console.log(maxOfTwoNumbers(10, 5));
 
-
-
-
-
-  
+console.log(maxOfThree(10,20,30));
